02b6839fc68ad3bfbe8ce105dba11f03ea0d0c3a
Fred Emmott <fred@fredemmott.co.uk>
2016-11-07 20:24:51 -0800

    Support class constants for attribute values
    refs #37

---
+++ src/expression/StaticClassClassConstantExpression.php
@@ -0,0 +1,24 @@
+<?hh // strict
+
+namespace FredEmmott\DefinitionFinder\Expression;
+
+use FredEmmott\DefinitionFinder\ClassConsumer;
+use FredEmmott\DefinitionFinder\TokenQueue;
+
+final class StaticClassClassConstantExpression extends Expression {
+  protected static function matchImpl(TokenQueue $tq): ?Expression {
+    $class = NamespaceStringExpression::match($tq);
+    if ($class === null) {
+      return null;
+    }
+    list ($t, $ttype) = $tq->shift();
+    if ($ttype !== T_DOUBLE_COLON) {
+      return null;
+    }
+    list ($t, $ttype) = $tq->shift();
+    if ($ttype !== T_CLASS) {
+      return null;
+    }
+    return new self($class->getValue());
+  }
+}
