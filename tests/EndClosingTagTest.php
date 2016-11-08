8e660c689453e09676b92195e3ed48fb2c90340a
Simon Welsh <simon@easyemployer.com>
2016-11-08 13:11:55 +1100

    Change consumeNonCode to a straight while loop.
    This handles cases where a closing PHP tag isn't followed by any tokens.
    Fixes #39

---
+++ tests/EndClosingTagTest.php
@@ -0,0 +1,11 @@
+<?hh // strict
+
+class EndClosingTagTest extends AbstractPHPTest {
+  protected function getFilename(): string {
+    return 'end_closing_tag.php';
+  }
+
+  protected function getPrefix(): string {
+    return '';
+  }
+}
---
+++ tests/data/end_closing_tag.php
@@ -0,0 +1,17 @@
+<?php
+
+class SimpleClass {
+}
+
+abstract class SimpleAbstractClass {
+}
+
+final class SimpleFinalClass {
+}
+
+interface SimpleInterface {
+}
+
+trait SimpleTrait {
+}
+?>
